50a5b4221ffb6018ccb584ee85c78f441c801a07
SaiPrabhu88 <gajavelly.prabhu88@gmail.com>
2024-10-06 19:51:32 +0530

    new code

--- main.tf
+++ main.tf
@@ -1,35 +1,42 @@
-resource "azurerm_resource_group" "aksrg" {
-  name     = "raks"
-  location = "centralindia"
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "aksResourceGrouprg"
+  location = "East US"
 }
-resource "azurerm_kubernetes_cluster" "akscls" {
-  name                = "aksclusteraks"
-  location            = azurerm_resource_group.aksrg.location
-  resource_group_name = azurerm_resource_group.aksrg.name
-  dns_prefix          = "akscluster1"
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "myAKSClusteraks"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "myaksdnnss"
 
   default_node_pool {
     name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D2_v2"
+    node_count = 1  # Adjust to your free-tier limits
+    vm_size    = "Standard_B2s"
   }
 
   identity {
     type = "SystemAssigned"
   }
 
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "Standard"
+  }
+
   tags = {
-    Environment = "Production"
+    environment = "development"
   }
 }
 
-output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.akscls.kube_config[0].client_certificate
-  sensitive = true
+resource "azurerm_kubernetes_cluster_node_pool" "additional_nodes" {
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
+  name                  = "additional"
+  node_count            = 1 
+  vm_size               = "Standard_B2s"
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.akscls.kube_config_raw
-
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
   sensitive = true
 }
