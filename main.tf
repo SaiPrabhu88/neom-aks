3e8cd32810423a3b4f4905b57a1fdd44a2051d70
SaiPrabhu88 <gajavelly.prabhu88@gmail.com>
2024-10-06 19:39:55 +0530

    changed resource group

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "azurerm_kubernetes_cluster" "akscls" {
 
   default_node_pool {
     name       = "default"
-    node_count = 2
+    node_count = 1
     vm_size    = "Standard_D2_v2"
   }
 
