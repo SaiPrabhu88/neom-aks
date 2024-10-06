c46eaccae8c4d7ce92f406834b357228b6f86458
SaiPrabhu88 <gajavelly.prabhu88@gmail.com>
2024-10-06 21:42:44 +0530

    changes done

--- provider.tf
+++ provider.tf
@@ -1,12 +1,23 @@
 terraform {
+  required_version = ">=1.0"
+
   required_providers {
-    azurerm = {
-        source = "registry.terraform.io/hashicorp/azurerm"
-        version = "~> 3.0.2"
-    }
     azapi = {
       source  = "azure/azapi"
       version = "~>1.5"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.9.1"
+    }
   }
 }
 provider "azurerm" {
